a2032f09c99bf4b0951d9fe7af908cf5468f9e5c
Luis <luiscdf@ua.pt>
2021-04-27 11:50:22 +0100

    gui bs

--- AS_OIS/src/ActiveEntity/AECustomer.java
+++ AS_OIS/src/ActiveEntity/AECustomer.java
@@ -1,6 +1,7 @@
 package ActiveEntity;
 
 
+import GUI.GUI_Manager;
 import SACorridor.ICorridor_Customer;
 import SAEntranceHall.IEntranceHall_Customer;
 import SAIdle.IIdle_Customer;
@@ -9,6 +10,9 @@ import SACorridorHall.ICorridorHall_Customer;
 import SAPaymentHall.IPaymentHall_Customer;
 import SAPaymentHall.SAPaymentHall;
 import SAPaymentPoint.IPaymentPoint_Customer;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -40,9 +44,13 @@ public class AECustomer extends Thread {
     // área partilhada PaymentPoint
     private final IPaymentPoint_Customer paymentPoint;
     
+//    GUI MANAGER
+    private GUI_Manager gUI_Manager;
+    
     public AECustomer( int customerId, IIdle_Customer idle, IOutsideHall_Customer outsideHall, 
                         IEntranceHall_Customer entranceHall, ICorridorHall_Customer[] corridorHall,
-                        ICorridor_Customer[] corridorShop, IPaymentHall_Customer paymentHall, IPaymentPoint_Customer paymentPoint ) {
+                        ICorridor_Customer[] corridorShop, IPaymentHall_Customer paymentHall, IPaymentPoint_Customer paymentPoint,
+                        GUI_Manager gUI_Manager) {
         this.customerId = customerId; 
         this.idle = idle;
         this.outsideHall = outsideHall;
@@ -51,6 +59,7 @@ public class AECustomer extends Thread {
         this.corridorShop = corridorShop;
         this.paymentHall = paymentHall;
         this.paymentPoint = paymentPoint;
+        this.gUI_Manager = gUI_Manager;
     }
     @Override
     public void run() {
@@ -59,9 +68,31 @@ public class AECustomer extends Thread {
             // idle.idle(customerId );
             // se simulação activa (não suspend, não stop, não end), thread avança para o outsideHall
             System.out.println(this.customerId + " entering Outside Hall");
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(AECustomer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             outsideHall.in( customerId );
+            gUI_Manager.enterOutsideHall(); // so sabemos k entrou quando entra
+            
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(AECustomer.class.getName()).log(Level.SEVERE, null, ex);
+            }
             System.out.println(this.customerId + " entering Entrance Hall");
+            
             entranceHall.in(customerId);
+            gUI_Manager.enterEntranceHall(customerId);  // so sabemos k entrou quando entra
+            
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(AECustomer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
             System.out.println(this.customerId + " left entrance hall");
             int curr_index=0;
             for (int curr=0; curr < corridorHall.length; curr++){
@@ -73,6 +104,14 @@ public class AECustomer extends Thread {
             System.out.println(this.customerId + " entering corridor hall num " + curr_index);
             if (corridorShop[curr_index].checkFull())
                 corridorHall[curr_index].in(customerId);
+            
+            gUI_Manager.enterCorridorHall(customerId, curr_index);
+            
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException ex) {
+                Logger.getLogger(AECustomer.class.getName()).log(Level.SEVERE, null, ex);
+            }
             
             corridorShop[curr_index].in(customerId, (SAPaymentHall) this.paymentHall);
             System.out.println(this.customerId + " shopping ");
